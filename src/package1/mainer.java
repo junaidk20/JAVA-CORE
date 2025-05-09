cb6b8a37b5512248b0df79ee363183a614ad10e3
junaidk20 <junaidkhan803061@gmail.com>
2025-05-09 17:02:26 +0530

    boolean operator

--- src/package1/mainer.java
+++ src/package1/mainer.java
@@ -5,10 +5,11 @@ class A{
 
 public class mainer {
     public static void main(String[] args) {
-        int a=10;
-        int b=20;
-       int c=(a>b)?a:b;
-        System.out.println(c);
+
+   boolean b=true|false;
+        boolean c=true&false;
+        System.out.println(b);
+
     }
 
 }
