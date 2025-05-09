7f3689be701efb1a3af63f016020fea3aa872433
junaidk20 <junaidkhan803061@gmail.com>
2025-05-09 17:32:31 +0530

    upcasting and downcasting

--- src/package1/mainer.java
+++ src/package1/mainer.java
@@ -1,17 +1,23 @@
 package package1;
+
+
 class A{
+  int Q=10;
+}
+class b extends A{
+    int P=Q;
 
 }
 
 public class mainer {
-    public static void main(String[] args) {
-    int c=0;
-    for (int i=0;i<10;i++){
-        c+=10;//arithmetic operator
+    public static void main(String[] args) throws Exception {
+        A a = new b();//upcasting
+        b bq=(b) a;//downcasting
+        System.out.println(bq.P);
+
+
 
     }
-        System.out.println(c);
 
-    }
 
 }
