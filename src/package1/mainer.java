f38dd53f1e62e3495ba879ff44e08fa2e189a8f7
junaidk20 <junaidkhan803061@gmail.com>
2025-05-09 16:56:09 +0530

    ternary operator

--- src/package1/mainer.java
+++ src/package1/mainer.java
@@ -1,14 +1,14 @@
 package package1;
 class A{
-   A getClasses(){
-     return this;
-    }
+
 }
 
 public class mainer {
     public static void main(String[] args) {
-        A a=new A();
-        System.out.println(a.getClasses());
+        int a=10;
+        int b=20;
+       int c=(a>b)?a:b;
+        System.out.println(c);
     }
 
 }
